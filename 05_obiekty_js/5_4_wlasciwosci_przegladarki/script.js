8dbcf12252fc92d4fae04f969ad3f7808b45bbc2
dstepniak <dor.stepniak@gmail.com>
2017-01-12 20:02:35 +0100

    Ex. 5.4. Objects JS - added js

---
+++ 05_obiekty_js/5_4_wlasciwosci_przegladarki/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load',function() {
+
+	// Variable storing button 'open'
+	var bopen = document.getElementById('open');
+
+	bopen.addEventListener('click',function() {
+		// Properties of object navigator
+		// appCodeName - name of browser application
+		var appCodeName = navigator.appCodeName;
+		// appVersion - version of browser application
+		var appVersion = navigator.appVersion;
+		// platform - platform for browser application
+		var platform = navigator.platform;
+		// cookieEnabled - informs whether cookie files are enabled
+		var cookieEnabled = navigator.cookieEnabled;
+		// userAgent - agent info: application name, version of application, platform, engine
+		var info = navigator.userAgent;
+		alert("Nazwa aplikacji: " + appCodeName);
+		alert("Wersja przeglądarki: " + appVersion);
+		alert("Platforma przeglądarki: " + platform);
+		alert("Włączona obsługa plików cookie: " + cookieEnabled);
+		alert("Dane przeglądarki: " + info);
+	});
+
+});
